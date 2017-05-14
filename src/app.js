9e07f145f0fc33a947e5c361813d4fb5e5851dac
John Balke <john.balke@live.com.au>
2017-05-14 12:11:40 +0100

    Added How-To on default route.

--- src/app.js
+++ src/app.js
@@ -7,12 +7,30 @@ var mongoURL = process.env.MONGOLAB_URI;
 
 const db = openDatabase(mongoURL);
 
-app.use('/api', searchApi);
+app.set('view engine', 'pug');
+app.set('views', './views');
 
+// Display Service How-To
 app.get('/', (req, res) => {
-    res.status(200).send('Image Search Abstractionb Layer Service');
+    res.status(200).render('index', {
+        title: 'Image Search Abstraction Layer',
+        app: 'FreeCodeCamp Basejump: Image Search Abstraction Layer',
+        usage: 'User Stories',
+        userStory1: 'I can get the image URLs, alt text and page urls for a set of images relating to a given search string.',
+        userStory2: 'I can paginate through the responses by adding a ?offset=2 parameter to the URL.',
+        userStory3: 'I can get a list of the most recently submitted search strings.',
+        section1: 'Usage Example',
+        content1: genFullUrl(req, "api/search/funny%20cats?offset=10"),
+        section2: 'Output Example',
+        content2: "{}",
+        section3: 'Usage Example',
+        content3: genFullUrl(req, "api/latest"),
+        section4: 'Will return recent queries:',
+        content4: '{}'
+    });
 });
 
+app.use('/api', searchApi);
 
 function openDatabase(url) {
     mongoose.Promise = global.Promise;
@@ -25,6 +43,16 @@ db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
     console.log('Database opened.')
 });
+
+// Generate a URL with current server hostname.
+function genFullUrl(req, url) {  
+    return `${req.protocol}://${req.hostname}:${getPort()}/${url}`;
+}
+
+// Get node.js port.
+function getPort() {  
+    return process.env.PORT || 3000;
+}
 
 module.exports.app = app;
 module.exports.mongoose = mongoose;
